3209e84f39013f3ed8adde91efc62af2ffdef31a
keanlim8 <keanlim58@gmail.com>
2021-01-29 19:08:46 -0500

    created signup page with firebase authentication

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBfHJYignUwhaaf3CgrSFVOvB3IqMcw7Ag",
+  authDomain: "clique-873f8.firebaseapp.com",
+  projectId: "clique-873f8",
+  storageBucket: "clique-873f8.appspot.com",
+  messagingSenderId: "536617280161",
+  appId: "1:536617280161:web:1bc2191b8140ac6ea946e0",
+  measurementId: "G-VJ332SCNCH"
+}
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig)
